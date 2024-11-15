7b3d1db963a8768d328bcfa651a19ee34ff98f99
Wans-G <sheng-wang-1@uiowa.edu>
2024-11-15 13:28:07 -0600

    sign up page and mechanism

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,9 @@ Rails.application.routes.draw do
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
+  # Register page
+  get '/register', to: 'users#new', as: 'register'
+
   # 3rd party login redirect
   get '/auth/:provider/callback', to: 'sessions#oauth_create'
   get '/auth/failure', to: redirect('/login')
