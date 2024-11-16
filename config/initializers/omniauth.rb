5205820c8d0000749f6983b8f2747891f0029e5c
Ziad Hasabrabu <zhasabrabu@uiowa.edu>
2024-11-16 00:05:55 -0600

    fixed link

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -7,7 +7,7 @@ OmniAuth.config.request_validation_phase = nil
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], {
     scope: 'email,profile',
-    redirect_uri: 'http://shards-of-the-grid-30472a19e6c9.herokuapp.com/auth/google_oauth2/callback'
+    redirect_uri: 'https://shards-of-the-grid-30472a19e6c9.herokuapp.com/auth/google_oauth2/callback'
   }
 end
 
