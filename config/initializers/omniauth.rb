41b34144798e13562cf51f861a3df9d79799bddb
Wans-G <sheng-wang-1@uiowa.edu>
2024-11-28 09:36:50 -0600

    bugfix-google login button and embedded env var format

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -7,7 +7,7 @@ OmniAuth.config.request_validation_phase = nil
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], {
     scope: 'email,profile',
-    redirect_uri: "https://#ENV['HOST_URL']/auth/google_oauth2/callback"
+    redirect_uri: "http://#{ENV['HOST_URL']}/auth/google_oauth2/callback"
   }
 end
 
