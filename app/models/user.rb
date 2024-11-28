2fcae04c0de28f171ba59d80bf96f1ffbb144f24
Wans-G <sheng-wang-1@uiowa.edu>
2024-11-28 10:08:48 -0600

    bug fix: add CSRF protection, loosen the name ReGex in registeration

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   before_save { |user| user.email=user.email.downcase }
   before_create :create_session_token
 
-  VALID_NAME_REGEX = /\A[^\s]+\z/
+  VALID_NAME_REGEX = /\A[^\s]+(\s[^\s]+)*\z/
   validates :name, presence: true, length: { in: 3..50 }, format: { with: VALID_NAME_REGEX }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true,
