e3bc3f53fb8c98d5fbe416c9fabc36696e749740
Wans-G <sheng-wang-1@uiowa.edu>
2024-11-28 22:03:31 -0600

    http/https change

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -7,7 +7,7 @@ OmniAuth.config.request_validation_phase = nil
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], {
     scope: 'email,profile',
-    redirect_uri: "#{ENV['HOST_URL']}/auth/google_oauth2/callback"
+    redirect_uri: "#{Rails.env.production? ? 'https' : 'http'}://#{ENV['HOST_URL']}/auth/google_oauth2/callback"
   }
 end
 
