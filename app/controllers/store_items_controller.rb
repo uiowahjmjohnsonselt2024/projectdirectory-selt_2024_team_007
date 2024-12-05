fc67b26a3db80ec36d2c62810a9a0505307ba6db
caallisn <caallisn@uiowa.edu>
2024-12-05 16:28:46 -0600

    fixing bug allowing users to input any amount of shards to purchase or spend they like, allowing negative balances and inifinite shards.

--- app/controllers/store_items_controller.rb
+++ app/controllers/store_items_controller.rb
@@ -9,14 +9,25 @@ class StoreItemsController < ApplicationController
     shard_amount = params[:shard_amount].to_i
 
     user = current_user  # Adjust based on how you get the current user
-    if shard_amount < 0
+
+    if shard_amount == 5 || shard_amount == 12 || shard_amount == 25
+
       user.update_column(:shards_balance, user.shards_balance + shard_amount)
       flash[:success] = "Purchase successful!"
       redirect_to store_items_path
-    elsif shard_amount > 0
-      user.update_column(:shards_balance, user.shards_balance + shard_amount)
-      flash[:success] = "Purchase successful!"
-      redirect_to store_items_path
+
+    elsif shard_amount < 5 && shard_amount > 0
+
+      if (user.shards_balance - shard_amount) > 0
+
+        user.update_column(:shards_balance, user.shards_balance - shard_amount)
+        flash[:success] = "Purchase successful!"
+        redirect_to store_items_path
+
+      else
+        flash[:danger] = "Insufficient Shard Balance"
+        redirect_to store_items_path
+      end
     end
   end
 
