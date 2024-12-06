7e04340db2a0710d18c46a0c02afb872c390fab3
Yusuf Halim <yusuf-halim@uiowa.edu>
2024-12-06 17:58:06 -0600

    added cvv field to billing methods table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_29_081407) do
+ActiveRecord::Schema[7.2].define(version: 2024_12_06_235525) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -46,6 +46,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_29_081407) do
     t.date "expiration_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "cvv", null: false
     t.index ["user_id"], name: "index_billing_methods_on_user_id"
   end
 
