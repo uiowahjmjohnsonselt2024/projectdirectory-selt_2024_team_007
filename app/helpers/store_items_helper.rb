580f0e4f3a7fc3c06fb6047e7dce99f9ea3feccf
Wans-G <sheng-wang-1@uiowa.edu>
2024-12-05 22:08:33 -0600

    fixed decimal display

--- app/helpers/store_items_helper.rb
+++ app/helpers/store_items_helper.rb
@@ -1,6 +1,7 @@
 module StoreItemsHelper
   def display_price(price_in_usd)
     converted_price = (price_in_usd * @exchange_rate).round(2)
-    "#{@currency} #{converted_price}"
+    formatted_price = format('%.2f', converted_price)
+    "#{@currency} #{formatted_price}"
   end
 end
