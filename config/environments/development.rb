aa80a54fc2cb4ba0dbe2a847c3298b013d507cc7
Ziad Hasabrabu <zhasabrabu@uiowa.edu>
2024-12-08 01:54:08 -0600

    Added live update of player list and navbar to game page

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,6 +41,7 @@ Rails.application.configure do
   # caching is enabled.
   config.action_mailer.perform_caching = false
 
+  Rails.application.routes.default_url_options[:host] = 'localhost:3000'
   config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
 
   # Print deprecation notices to the Rails logger.
