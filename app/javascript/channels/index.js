c7a56826bb30cd38477a9d7b166969846e4bc65e
Bradley Johnson <90875992+bjohnson66@users.noreply.github.com>
2024-12-11 07:55:05 -0600

    Added missing import

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -1,3 +1,4 @@
 // Import all the channels to be used by Action Cable
-import "./chat_channel"
-import "./presence_channel"
+import "./consumer";
+import "./chat_channel";
+import "./presence_channel";
