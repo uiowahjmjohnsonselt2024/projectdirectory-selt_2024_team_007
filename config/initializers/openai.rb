24f18c8a7478fcdcac3f4b9205f28c54a2696187
Bradley Johnson <90875992+bjohnson66@users.noreply.github.com>
2024-12-11 04:45:19 -0600

    Fixed formatting of game page, Changed background of settings and game pages Fixed cucumber tests

--- config/initializers/openai.rb
+++ config/initializers/openai.rb
@@ -1,6 +1,6 @@
-require 'openai'
+require "openai"
 
 OpenAI.configure do |config|
-  config.access_token = ENV.fetch("OPEN_AI_API_KEY_SHARDS_OF_THE_GRID")
+  config.access_token = ENV.fetch('OPEN_AI_API_KEY_SHARDS_OF_THE_GRID', 'fake_test_api_key')
   config.log_errors = true # Recommended for development to debug issues
 end
