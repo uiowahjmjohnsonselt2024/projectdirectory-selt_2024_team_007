5236d20de949b1a01d2356266024b76ba6c4dcdc
Ziad Hasabrabu <zhasabrabu@uiowa.edu>
2024-12-11 20:09:57 -0600

    heroku attempted fix 11

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,8 @@
 require 'redis'
 $redis = Redis.new(url: ENV['REDIS_URL'] || 'redis://localhost:6379/1')
+
+Redis.new(
+  url: 'url',
+  driver: :ruby,
+  ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
+)
