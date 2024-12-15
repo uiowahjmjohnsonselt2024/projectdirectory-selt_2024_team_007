5df153739105dedd636e87df765c69136144c8d6
Ziad Hasabrabu <zhasabrabu@uiowa.edu>
2024-12-15 02:09:22 -0600

    Added AI disclaimer to Spec Files

--- spec/factories/store_item.rb
+++ spec/factories/store_item.rb
@@ -1,3 +1,19 @@
+# *********************************************************************
+# This file was crafted using assistance from Generative AI Tools. 
+# Open AI's ChatGPT o1, 4o, and 4o-mini models were used from November 
+# 4th 2024 to December 15, 2024. The AI Generated code was not 
+# sufficient or functional outright nor was it copied at face value. 
+# Using our knowledge of software engineering, ruby, rails, web 
+# development, and the constraints of our customer, SELT Team 007 
+# (Cody Alison, Yusuf Halim, Ziad Hasabrabu, Bradley Johnson, 
+# and Sheng Wang) used GAITs responsibly; verifying that each line made
+# sense in the context of the app, conformed to the overall design, 
+# and was testable. We maintained a strict peer review process before
+# any code changes were merged into the development or production 
+# branches. All code was tested with BDD and TDD tests as well as 
+# empirically tested with local run servers and Heroku deployments to
+# ensure compatibility.
+# *******************************************************************
 FactoryBot.define do
   factory :store_item do
     name { Faker::Commerce.product_name }
